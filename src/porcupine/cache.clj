ebddb40715c2d34126a7fdc97f33b4b9e8995758
Kyle Dawkins <kdawkins@fastmail.fm>
2013-11-30 02:16:25 -0800

    Adding a cheesy simple cache for caching files internally

---
+++ src/porcupine/cache.clj
@@ -0,0 +1,40 @@
+(ns porcupine.cache
+  (:import java.util.Date))
+
+;;; TODO - fix this!
+(defprotocol Cache
+  (cget [this key])
+  (has? [this key])
+  (purge [this])
+  (expired? [this key])
+  (cset [this key val] [this key val opts]))
+
+(defrecord MapCache [a]
+  Cache
+  (cget [c key]
+    (when-not (expired? c key)
+      (-> @a :cache key)))
+  (expired? [c key]
+    (if-let [exp (-> @a :timeout key)]
+      (let [now (Date.)
+            seconds (.getTime now)]
+        (> seconds exp))
+      false))
+  (has? [this key]
+    (contains? (-> @a :cache) key))
+  (purge [this]
+    (reset! a {:cache {} :timeout {}}))
+  (cset [c key value]
+    (cset c key value {}))
+  (cset [c key value opts]
+    (swap! a assoc-in [:cache key] value)
+    (if-let [timeout (:timeout opts)]
+      (let [now (Date.)
+            seconds (.getTime now)
+            expiration (+ seconds (* 1000 timeout))]
+        (swap! a assoc-in [:timeout key] expiration))
+      (swap! a assoc-in [:timeout key] nil))))
+
+
+(defn map-cache []
+  (MapCache. (atom {})))

744b3520e372822dfc46d91489bcc85dc5aa0b56
Kyle Dawkins <kdawkins@fastmail.fm>
2013-12-06 00:29:50 -0800

    Uglification works now - as a test.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.flatland/ordered "1.5.2"]
-                 [com.yahoo.platform.yui/yuicompressor "2.4.2"]
-                 [ring/ring-core "1.2.1"]])
+                 [ring/ring-core "1.2.1"]
+                 [ro.isdc.wro4j/wro4j-extensions "1.7.2"]])
--- src/porcupine/transforms.clj
+++ src/porcupine/transforms.clj
@@ -1,4 +1,8 @@
-(ns porcupine.transforms)
+(ns porcupine.transforms
+  (:import ro.isdc.wro.model.resource.Resource
+           ro.isdc.wro.extensions.processor.js.UglifyJsProcessor
+           java.io.StringReader
+           java.io.StringWriter))
 
 ;; functions that transform resources in different ways.
 ;; these functions must take a vector of resources
@@ -7,3 +11,26 @@
 (defn identity
   "Returns its input."
   [& resources] resources)
+
+;; TODO - these need a protocol
+
+(defn uglify-js-processor []
+  (UglifyJsProcessor.))
+
+
+(defn uglify-resource
+  [resource]
+  (let [processor (uglify-js-processor)
+        writer (StringWriter.)]
+    (.process processor
+              (Resource.)
+              (StringReader. (:contents resource))
+              writer)
+    (str (.getBuffer writer))))
+
+(defn uglify
+  "Uglifies its input."
+  [& resources]
+  (let [out (map uglify-resource resources)]
+    (into [] (for [o out r resources]
+               (assoc r (:contents o))))))

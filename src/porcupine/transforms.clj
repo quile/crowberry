5e439124eddbe5c8916c2b32018847ee390722a2
Kyle Dawkins <kdawkins@fastmail.fm>
2013-12-07 00:55:02 -0800

    More transform cleanup

--- src/porcupine/transforms.clj
+++ src/porcupine/transforms.clj
@@ -1,8 +1,23 @@
 (ns porcupine.transforms
   (:import ro.isdc.wro.model.resource.Resource
            ro.isdc.wro.extensions.processor.js.UglifyJsProcessor
+           ro.isdc.wro.extensions.processor.js.JsLintProcessor
            java.io.StringReader
            java.io.StringWriter))
+
+(defn transform-all
+  [f resources]
+  (let [out (map f resources)]
+    (into [] (map (fn [r o] (assoc r :contents o)) resources out))))
+
+(defn process
+  [resource processor]
+  (let [writer (StringWriter.)]
+    (.process processor
+              (Resource.)
+              (StringReader. (:contents resource))
+              writer)
+    (str (.getBuffer writer))))
 
 ;; functions that transform resources in different ways.
 ;; these functions must take a vector of resources
@@ -10,27 +25,31 @@
 
 (defn identity
   "Returns its input."
-  [& resources] resources)
+  [resources] resources)
+
 
 ;; TODO - these need a protocol
 
-(defn uglify-js-processor []
-  (UglifyJsProcessor.))
+(def uglify-js-processor (UglifyJsProcessor.))
+(def jslint-processor (JsLintProcessor.))
 
 
 (defn uglify-resource
   [resource]
-  (let [processor (uglify-js-processor)
-        writer (StringWriter.)]
-    (.process processor
-              (Resource.)
-              (StringReader. (:contents resource))
-              writer)
-    (str (.getBuffer writer))))
+  (process resource uglify-js-processor))
+
 
 (defn uglify
   "Uglifies its input."
-  [& resources]
-  (let [out (map uglify-resource resources)]
-    (into [] (for [o out r resources]
-               (assoc r (:contents o))))))
+  [resources]
+  (transform-all uglify-resource resources))
+
+
+(defn jslint-resource
+  [resource]
+  (process resource jslint-processor))
+
+(defn jslint
+  "Runs jslint on its input."
+  [resources]
+  (transform-all jslint-resource resources))

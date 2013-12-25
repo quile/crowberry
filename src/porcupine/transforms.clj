9d08324412389bb2c1a82fb7a6f298f2baabc24e
Kyle Dawkins <kdawkins@fastmail.fm>
2013-12-24 23:11:06 -0800

    More work on transforms (mostly in vain)

--- src/porcupine/transforms.clj
+++ src/porcupine/transforms.clj
@@ -2,6 +2,7 @@
   (:import ro.isdc.wro.model.resource.Resource
            ro.isdc.wro.extensions.processor.js.UglifyJsProcessor
            ro.isdc.wro.extensions.processor.js.JsLintProcessor
+           ro.isdc.wro.model.resource.processor.impl.js.ConsoleStripperProcessor
            java.io.StringReader
            java.io.StringWriter))
 
@@ -10,11 +11,20 @@
   (let [out (map f resources)]
     (into [] (map (fn [r o] (assoc r :contents o)) resources out))))
 
+(defn fake-resource
+  [resource]
+  (let [ty (get-in resource [:opts :type])]
+    (cond
+      (= ty :javascript) "foo.js"
+      (= ty :stylesheet) "bar.css"
+      (= ty :icon)       "baz.ico"
+      :else              "quux.js")))
+
 (defn process
   [resource processor]
   (let [writer (StringWriter.)]
     (.process processor
-              (Resource.)
+              (Resource/create (fake-resource resource))
               (StringReader. (:contents resource))
               writer)
     (str (.getBuffer writer))))
@@ -27,23 +37,20 @@
   "Returns its input."
   [resources] resources)
 
-
 ;; TODO - these need a protocol
 
 (def uglify-js-processor (UglifyJsProcessor.))
 (def jslint-processor (JsLintProcessor.))
-
+(def console-stripper-processor (ConsoleStripperProcessor.))
 
 (defn uglify-resource
   [resource]
   (process resource uglify-js-processor))
 
-
 (defn uglify
   "Uglifies its input."
   [resources]
   (transform-all uglify-resource resources))
-
 
 (defn jslint-resource
   [resource]
@@ -53,3 +60,12 @@
   "Runs jslint on its input."
   [resources]
   (transform-all jslint-resource resources))
+
+(defn console-strip-resource
+  [resource]
+  (process resource console-stripper-processor))
+
+(defn console-stripper
+  "Strips console.* from JS"
+  [resources]
+  (transform-all console-strip-resource resources))
